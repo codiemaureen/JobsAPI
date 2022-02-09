a5c173102f36d85990260dd7c6e3fbb0475471e9
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-09 12:56:32 -0500

    Adding Swagger Docs/Hosting on Heroku

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -31,7 +36,14 @@ app.use(cors());
 app.use(xss());
 // extra packages
 
+
 // routes
+app.get('/', (req,res) => {
+  res.send('<h1>Welcome to the Job Tracking API</h1><a href="/api-docs">Documentation</a>')
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
